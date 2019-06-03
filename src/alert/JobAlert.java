192ed81ab599da010fea719f3d51edaf7ea80e0c
NathanaelGastelum <34591895+NathanaelGastelum@users.noreply.github.com>
2019-06-03 14:45:00 -0700

    Add files via upload

---
+++ src/alert/JobAlert.java
@@ -0,0 +1,34 @@
+package alert;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class JobAlert {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","src\\alert\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.setHeadless(true);
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://zwift.com/careers/");
+		driver.switchTo().frame("grnhse_iframe");
+		
+		// Find the QA job postings
+        List<WebElement> jobs = driver.findElements(By.partialLinkText("QA"));
+        
+        //Check number of new QA postings
+        System.out.println(jobs.size() + " Job Postings:"); //TODO add a condition for checking if the posting is new and in Long Beach
+		
+        //Print each job posting
+        for (int i=0; i<jobs.size();i++) {
+        	System.out.println(jobs.get(i).getText());
+        }
+        
+        //Close the browser
+        driver.quit();
+	}
+}
